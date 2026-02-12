3a7fbcf701ed8f602d3d242650f2a229b3e47188
kelvinnzyoki <kelvinnzyokimaitha@gmail.com>
2026-02-12 14:54:12 +0300

    Update server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ dotenv.config();
 // ==========================================
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT;
 
 // Initialize Prisma
 const prisma = new PrismaClient({
